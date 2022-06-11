6260819966de68f86167b0f3ce7b35006e93ad4e
Ikrom <ikromyusupov09@gmail.com>
2022-06-11 15:47:22 +0500

    Find how many numbers it contains and return

--- While01.py
+++ While01.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count = 0
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+            count += 1
+        i += 1
+    return count

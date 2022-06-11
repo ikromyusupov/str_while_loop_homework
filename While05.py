b3a0593eeb80f355b530afc5ac53a6f5b0d58e98
Ikrom <ikromyusupov09@gmail.com>
2022-06-11 16:17:34 +0500

    Find how many lowercase letters there are and return.

--- While05.py
+++ While05.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    j = 0
+    while i < len(s):
+        if s[i].islower():
+            j+=1    
+        i+=1
+    return j
